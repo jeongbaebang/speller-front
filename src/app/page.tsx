9b95e219f7c2c25c5f9ea6e175c9d0ddb6665857
suhyeoonn <suhyeon1740@gmail.com>
2024-11-15 12:54:25 +0900

    ➕ Install shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -20,7 +21,9 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>
+            Save and see your changes instantly.<Button>click</Button>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
