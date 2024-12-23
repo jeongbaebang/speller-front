5bb615169641ccc3515b6b70daf0f05e6b2bce66
박종찬 <chanii8@naver.com>
2024-12-23 13:33:32 +0900

    feat: 헤더 컴포넌트 반응형 작업

--- src/shared/ui/header.tsx
+++ src/shared/ui/header.tsx
@@ -4,24 +4,52 @@ import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 const Header = () => {
   return (
-    <header>
-      <h1>한국어 맞춤법 검사기</h1>
+    <header className="flex items-center justify-between p-[1.125rem_1.5625rem] pc:p-[2rem_3.75rem]">
+      <div className="flex items-center gap-[1.25rem]">
+        <h1 className="text-[1.125rem] font-[700]">한국어 맞춤법 검사기</h1>
+        <Link href="/" className="hidden tab:inline-flex p-[0.625rem_0.75rem]">
+          이전 버전 사용하기
+        </Link>
+      </div>
+      <div className="hidden pc:flex items-center gap-[1rem]">
+        <Link href="/" className="tab:inline-flex p-[0.625rem_0.75rem]">
+          사용법
+        </Link>
+        <Link href="/" className="tab:inline-flex p-[0.625rem_0.75rem]">
+          문의하기
+        </Link>
+      </div>
       <Popover>
         <PopoverTrigger asChild>
-          <Button>
+          <Button className="pc:hidden w-[1.5rem] h-[1.5rem] p-0 bg-icon-menu bg-no-repeat bg-center bg-[length:1.25rem] hover:bg-accent">
             <span className="sr-only">메뉴보기</span>
           </Button>
         </PopoverTrigger>
-        <PopoverContent>
+        <PopoverContent className="p-0 w-auto rounded-lg">
           <ul>
-            <li>
-              <Link href="/">이전 버전 사용하기</Link>
+            <li className="tab:hidden">
+              <Link
+                href="/"
+                className="block p-[0.75rem] text-[0.875rem] leading-none"
+              >
+                이전 버전 사용하기
+              </Link>
             </li>
             <li>
-              <Link href="/">사용법</Link>
+              <Link
+                href="/"
+                className="block p-[0.75rem] text-[0.875rem] leading-none"
+              >
+                사용법
+              </Link>
             </li>
             <li>
-              <Link href="/">문의하기</Link>
+              <Link
+                href="/"
+                className="block p-[0.75rem] text-[0.875rem] leading-none"
+              >
+                문의하기
+              </Link>
             </li>
           </ul>
         </PopoverContent>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,6 +81,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      backgroundImage: {
+        "icon-menu": "url('/images/icon-menu.svg')",
+      },
     },
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
