134eac65bd1e1b16e361a6ce4bb88eba6e310a87
박종찬 <chanii8@naver.com>
2024-12-30 10:58:46 +0900

    chore: 헤더 컴포넌트 bg class name 추가

--- src/shared/ui/header.tsx
+++ src/shared/ui/header.tsx
@@ -4,28 +4,28 @@ import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-[1.125rem_1.5625rem] pc:p-[2rem_3.75rem]">
+    <header className="flex items-center justify-between bg-white p-[1.125rem_1.5625rem] pc:p-[2rem_3.75rem]">
       <div className="flex items-center gap-[1.25rem]">
         <h1 className="text-[1.125rem] font-[700]">한국어 맞춤법 검사기</h1>
-        <Link href="/" className="hidden tab:inline-flex p-[0.625rem_0.75rem]">
+        <Link href="/" className="hidden p-[0.625rem_0.75rem] tab:inline-flex">
           이전 버전 사용하기
         </Link>
       </div>
-      <div className="hidden pc:flex items-center gap-[1rem]">
-        <Link href="/" className="tab:inline-flex p-[0.625rem_0.75rem]">
+      <div className="hidden items-center gap-[1rem] pc:flex">
+        <Link href="/" className="p-[0.625rem_0.75rem] tab:inline-flex">
           사용법
         </Link>
-        <Link href="/" className="tab:inline-flex p-[0.625rem_0.75rem]">
+        <Link href="/" className="p-[0.625rem_0.75rem] tab:inline-flex">
           문의하기
         </Link>
       </div>
       <Popover>
         <PopoverTrigger asChild>
-          <Button className="pc:hidden w-[1.5rem] h-[1.5rem] p-0 bg-icon-menu bg-no-repeat bg-center bg-[length:1.25rem] hover:bg-accent">
+          <Button className="h-[1.5rem] w-[1.5rem] bg-icon-menu bg-[length:1.25rem] bg-center bg-no-repeat p-0 hover:bg-accent pc:hidden">
             <span className="sr-only">메뉴보기</span>
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="p-0 w-auto rounded-lg">
+        <PopoverContent className="w-auto rounded-lg p-0">
           <ul>
             <li className="tab:hidden">
               <Link
