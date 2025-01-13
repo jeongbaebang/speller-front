d14dd598399f8a24416efdec546a31ac19602529
suhyeoonn <suhyeon1740@gmail.com>
2025-01-13 12:35:36 +0900

    rename: /speller/result 하위 파일 /results로 경로 수정 (#30)

--- app/speller/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { BaseLayout } from '@/shared/ui/base-layout'
-
-const SpellerLayout = ({ children }: { children: React.ReactNode }) => {
-  return <BaseLayout footerLayout='default'>{children}</BaseLayout>
-}
-
-export default SpellerLayout
--- app/speller/result/page.tsx
+++
@@ -1,3 +0,0 @@
-import { ResultPage } from '@/pages/result/ui/result-page'
-
-export default ResultPage
