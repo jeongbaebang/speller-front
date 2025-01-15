6b9723a434f33e7f45fa22f97c8c075e86cb278b
jeongbaebang <jeongbaebang_dev@naver.com>
2025-01-15 16:05:23 +0900

    design: 강한 검사 스위치 스타일 조정 (#24)

--- src/pages/speller/ui/speller-setting.tsx
+++ src/pages/speller/ui/speller-setting.tsx
@@ -11,10 +11,10 @@ interface SpellerSettingProps {
 const SpellerSetting: FC<SpellerSettingProps> = memo(
   ({ checked, onCheckedChange }) => {
     return (
-      <div className='mb-2 mt-[1.13rem] flex items-center justify-end gap-2 tab:mb-[1.38rem] tab:mt-[1.75rem]'>
+      <div className='mb-2 mt-[0.94rem] flex items-center justify-end gap-2 tab:mb-[1.38rem] tab:mt-[1.75rem]'>
         <Label
           htmlFor='airplane-mode'
-          className='self-center text-[0.9375rem] font-medium tracking-[-0.01875rem] text-slate-600'
+          className='self-center text-[0.9375rem] font-medium leading-[1.40625rem] tracking-[-0.01875rem] text-slate-600'
         >
           강한 검사
         </Label>
