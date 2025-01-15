7e9cbaef09f929c19532b2a3b875bdc2adda6511
jeongbaebang <jeongbaebang_dev@naver.com>
2025-01-15 18:00:23 +0900

    design: 테블릿 원문 컨트롤 컴포넌트 수치 조정 (#24)
    - 그라디언트바 세로 크기 변경
    - 텍스트 카운트 수치 조정

--- src/pages/speller/ui/speller-control.tsx
+++ src/pages/speller/ui/speller-control.tsx
@@ -14,7 +14,7 @@ const SpellerControl: FC<SpellerControlProps> = ({ count, isSubmitted }) => {
   const isButtonDisabled = count <= 0 || isSubmitted
 
   return (
-    <div className='mt-2 flex flex-shrink-0 justify-between'>
+    <div className='mt-2 flex flex-shrink-0 justify-between tab:mt-[0.625rem]'>
       <TextCounter count={count} />
       <Button type='submit' disabled={isButtonDisabled}>
         {isSubmitted ? '검사중...' : '검사하기'}
--- src/shared/ui/text-counter.tsx
+++ src/shared/ui/text-counter.tsx
@@ -6,7 +6,7 @@ interface TextCounterProps {
 
 const TextCounter: FC<TextCounterProps> = ({ count }) => {
   return (
-    <p className='self-end text-[0.875rem] font-medium tracking-[-0.0175rem] text-slate-300'>
+    <p className='self-end text-[0.875rem] font-medium leading-[1.75rem] tracking-[-0.0175rem] text-slate-300'>
       {count}자
     </p>
   )
