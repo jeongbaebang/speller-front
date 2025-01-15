70f8c4529b07548dd80ee05b60782e0d16731fe7
jeongbaebang <jeongbaebang_dev@naver.com>
2025-01-16 02:15:45 +0900

    design: pc 강한 검사 텍스트 자간 조정 (#24)

--- src/pages/results/ui/strong-check-message.tsx
+++ src/pages/results/ui/strong-check-message.tsx
@@ -8,7 +8,7 @@ const StrongCheckMessage = () => {
   } = useSpeller()
 
   return (
-    <div className='mb-[0.44rem] mt-[1.12rem] flex items-center justify-end gap-2 text-right text-[1rem] font-medium leading-[1.5rem] tracking-[-0.02rem] text-slate-300 tab:mb-[0.5rem] tab:mt-[1.94rem]'>
+    <div className='mb-[0.44rem] mt-[1.12rem] flex items-center justify-end gap-2 text-right text-[1rem] font-medium leading-[150%] tracking-[-0.02rem] text-slate-300 tab:mb-[0.5rem] tab:mt-[1.94rem] pc:mb-[0.81rem] pc:mt-[2rem] pc:text-[1.25rem] pc:tracking-[-0.025rem]'>
       {requestedWithStrictMode
         ? '강한 검사가 적용되었습니다.'
         : '강한 검사가 적용되지 않았습니다.'}
