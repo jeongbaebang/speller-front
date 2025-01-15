084ab41cc70bf88a00b064cac373912c5fdd3cf8
jeongbaebang <jeongbaebang_dev@naver.com>
2025-01-15 23:59:10 +0900

    design: 강한 검사 텍스트 레이아웃 조정 (#24)

--- src/pages/results/ui/strong-check-message.tsx
+++ src/pages/results/ui/strong-check-message.tsx
@@ -8,7 +8,7 @@ const StrongCheckMessage = () => {
   } = useSpeller()
 
   return (
-    <div className='mb-2 mt-[1.13rem] flex items-center justify-end gap-2 text-right text-[0.9375rem] font-medium leading-[1.40625rem] tracking-[-0.01875rem] text-slate-300 tab:mb-[1.38rem] tab:mt-[1.75rem]'>
+    <div className='mb-[0.44rem] mt-[1.12rem] flex items-center justify-end gap-2 text-right text-[1rem] font-medium leading-[1.5rem] tracking-[-0.02rem] text-slate-300 tab:mb-[1.38rem] tab:mt-[1.75rem]'>
       {requestedWithStrictMode
         ? '강한 검사가 적용되었습니다.'
         : '강한 검사가 적용되지 않았습니다.'}
