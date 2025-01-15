bfa36327de424bcfdcbe18a12009f87d4e28cdca
jeongbaebang <jeongbaebang_dev@naver.com>
2025-01-15 16:09:07 +0900

    design: 검사하기 버튼 상하 간격 조정 (#24)

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-[3.375rem] px-[1.94rem] py-[0.63rem]',
+        default: 'h-[3.375rem] px-[1.94rem] py-[0.62rem]',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
