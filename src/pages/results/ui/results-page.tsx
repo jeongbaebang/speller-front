92eb60429ebf225d372ffe8ff6e3f6cbc254da8a
bicochan <chanii8@naver.com>
2025-01-21 00:34:42 +0900

    style: 맞춤법/문법 오류 섹션 스타일 수정 (#41)

--- src/pages/results/ui/results-page.tsx
+++ src/pages/results/ui/results-page.tsx
@@ -17,7 +17,7 @@ const ResultsPage = () => {
           <ResultsControl />
         </div>
         {/* 맞춤법/문법 오류 */}
-        <div className='flex max-h-[30.5rem] min-h-[30.5rem] w-full flex-1 flex-col rounded-lg border border-blue-500 bg-white px-5 pb-3.5 pt-[18px] tab:rounded-[1rem] tab:p-10 tab:pb-7 pc:max-h-[40.25rem] pc:rounded-bl-none pc:rounded-tl-none pc:border-none'>
+        <div className='flex max-h-[30.5rem] min-h-[30.5rem] w-full flex-1 flex-col rounded-lg border border-blue-500 bg-white px-5 pb-3.5 pt-[1.125rem] tab:rounded-[1rem] tab:p-10 tab:pb-7 pc:max-h-[40.25rem] pc:rounded-bl-none pc:rounded-tl-none pc:border-none pc:pb-10'>
           <ErrorTrackingSection />
         </div>
       </div>
