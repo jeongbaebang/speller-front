ec8cc0cd6f13cc86d0ad3334b7ca59631f529184
Park JongChan <chanii8@finenuts.co.kr>
2025-03-23 10:39:30 +0900

    chore: 구글 애드 센스 entities에서 shared로 이동 (#150)

--- src/entities/google-ad-sense/ui/footer-ad.tsx
+++
@@ -1,9 +0,0 @@
-import GoogleAdSense from './google-ad-sense'
-
-const FooterAd = () => {
-  return (
-    <GoogleAdSense className='mb-1 w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:w-full pc:w-1/2' />
-  )
-}
-
-export { FooterAd }
