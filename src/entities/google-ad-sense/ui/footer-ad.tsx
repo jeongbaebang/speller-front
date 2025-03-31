d1c6bee29c692bd4850207504d0513b68b92ba39
suhyeoonn <suhyeon1740@gmail.com>
2025-04-01 05:39:52 +0900

    fix: 푸터 광고에 slot 및 반응형 광고 속성 추가 (#154)

--- src/entities/google-ad-sense/ui/footer-ad.tsx
+++ src/entities/google-ad-sense/ui/footer-ad.tsx
@@ -2,7 +2,11 @@ import GoogleAdSense from './google-ad-sense'
 
 const FooterAd = () => {
   return (
-    <GoogleAdSense className='mb-1 h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px]' />
+    <GoogleAdSense
+      className='mb-1 h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px]'
+      data-ad-slot='9725653724'
+      data-full-width-responsive='true'
+    />
   )
 }
 
