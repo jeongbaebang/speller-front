9ed4169db4d369946cff9139b813e6eede61a27f
suhyeoonn <suhyeon1740@gmail.com>
2025-04-05 07:24:52 +0900

    fix: 데스크탑에만 ins 태그 생성되도록 수정 (#154)

--- src/entities/google-ad-sense/ui/footer-ad.tsx
+++ src/entities/google-ad-sense/ui/footer-ad.tsx
@@ -3,7 +3,7 @@ import GoogleAdSense from './google-ad-sense'
 const FooterAd = () => {
   return (
     <GoogleAdSense
-      className='mb-1 h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px]'
+      className='mb-1 hidden h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px] pc:block'
       data-ad-slot='4790060150'
       data-full-width-responsive='true'
     />
