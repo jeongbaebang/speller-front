ac8b20e87e102f175e1dad1d3fd1613579dc4a7f
suhyeoonn <suhyeon1740@gmail.com>
2025-04-05 06:22:25 +0900

    fix: footer 광고 slot 변경 (#154)

--- src/entities/google-ad-sense/ui/footer-ad.tsx
+++ src/entities/google-ad-sense/ui/footer-ad.tsx
@@ -4,7 +4,7 @@ const FooterAd = () => {
   return (
     <GoogleAdSense
       className='mb-1 h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px]'
-      data-ad-slot='9725653724'
+      data-ad-slot='4790060150'
       data-full-width-responsive='true'
     />
   )
